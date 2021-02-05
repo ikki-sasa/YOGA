9f3134872174bcddb3e2eb5854d96adee4bed5be
ikki-sasa <manga.samadjine@3wa.io>
2021-02-05 17:07:36 +0100

    buttin-scroll-js-ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// BUTTON SCROLL
+const scrollBtn = document.querySelector('.scroll-btn');
+
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+       scrollBtn.style.display = 'block'; 
+    }
+    else {
+        scrollBtn.style.display = 'none'; 
+
+    }
+})
+scrollBtn.addEventListener('click', () => {
+    window.scroll({
+        top: 0 ,
+        behavior: "smooth"
+    })
+})
+// BUTTON SCROLL END
 
 // let MenuItems = document.getElementById("MenuItems");
 // MenuItems.style.maxHeight = "0px";
