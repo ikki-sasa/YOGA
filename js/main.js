4df7139db4a5d6db89ed8fe4f4e71d1282387fc2
ikki-sasa <manga.samadjine@3wa.io>
2021-02-05 11:34:44 +0100

    practice-page

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+// let MenuItems = document.getElementById("MenuItems");
+// MenuItems.style.maxHeight = "0px";
+// function menutoggle() {
+//     if(MenuItems.style.maxHeight === "0px") 
+//     {
+//         MenuItems.style.maxHeight = "200px";
+//     }
+//     else 
+//     {
+//         MenuItems.style.maxHeight = "0px";
+
+//     }
+// }
+// document.getElementById("MenuItems").onclick = function() {menutoggle()};
+
+// function menutoggle() {
+//   document.getElementById("MenuItems").innerHTML = "";
+// }
