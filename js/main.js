2a31b8cfe298f0ec5527a6ea8f8785f22f40e09b
ikki-sasa <manga.samadjine@3wa.io>
2021-02-08 10:56:25 +0100

    JS-modif

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// BUTTON SCROLL
+// BUTTON SCROLL START
 const scrollBtn = document.querySelector('.scroll-btn');
 
 window.addEventListener('scroll', () => {
@@ -18,21 +18,39 @@ scrollBtn.addEventListener('click', () => {
 })
 // BUTTON SCROLL END
 
-// let MenuItems = document.getElementById("MenuItems");
-// MenuItems.style.maxHeight = "0px";
-// function menutoggle() {
-//     if(MenuItems.style.maxHeight === "0px") 
-//     {
-//         MenuItems.style.maxHeight = "200px";
-//     }
-//     else 
-//     {
-//         MenuItems.style.maxHeight = "0px";
+// JS FOR TOGGLE MENU START
+    let MenuItems = document.getElementById("MenuItems");
+      MenuItems.style.maxHeight = "0px";
+      function menutoggle() {
+if(MenuItems.style.maxHeight === "0px") 
+  {
+      MenuItems.style.maxHeight = "200px";
+  }
+  else 
+  {
+      MenuItems.style.maxHeight = "0px";
 
-//     }
-// }
-// document.getElementById("MenuItems").onclick = function() {menutoggle()};
+  }
+}
+// JS FOR TOGGLE MENU END
 
-// function menutoggle() {
-//   document.getElementById("MenuItems").innerHTML = "";
-// }
+// JS FOR TOGGLE FORM START
+
+  
+  let LoginForm = document.getElementById("LoginForm");
+  let RegForm = document.getElementById("RegForm");
+  let Indicator = document.getElementById("Indicator");
+
+  function register() {
+      RegForm.style.transform = "translateX(0px)";
+      LoginForm.style.transform = "translateX(0px)";
+      Indicator.style.transform = "translateX(100px)";
+  }
+
+  function login() {
+      RegForm.style.transform = "translateX(300px)";
+      LoginForm.style.transform = "translateX(300px)";
+      Indicator.style.transform = "translateX(0px)";
+  }
+// JS FOR TOGGLE FORM ENDS 
+
